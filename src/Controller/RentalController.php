4cc8861098087b970cbb55701a5e6c66bc78cbf6
jbonnet <jbonnet@jobinlive.fr>
2026-01-29 22:00:45 +0000

    Update: Titres produits location-materiel, descriptions avec points, icône placeholder FontAwesome, correction boucle infinie images

--- src/Controller/RentalController.php
+++ src/Controller/RentalController.php
@@ -45,10 +45,10 @@ class RentalController extends AbstractController
         return [
             // SONORISATION
             'midas-m32r-live' => [
-                'name' => 'MIDAS M32R Live',
+                'name' => 'Console numérique MIDAS M32R Live',
                 'slug' => 'midas-m32r-live',
                 'category' => 'Sonorisation',
-                'short_description' => 'Console numérique compacte et flexible, jusqu\'à 32 entrées',
+                'short_description' => 'Console numérique compacte et flexible, jusqu\'à 32 entrées.',
                 'description' => 'La M32R-LIVE est une console numérique 16+1 fadeurs motorisés, offrant 48 voies vers 25 bus de mixage. Idéale pour la sonorisation et l\'enregistrement sur scène ou en studio lorsque l\'encombrement doit être réduit sans concession aux performances.',
                 'image' => '/IRCI/Midas M32R Live.jpg',
                 'features' => [
@@ -63,10 +63,10 @@ class RentalController extends AbstractController
                 ],
             ],
             'jbl-eon-610-710' => [
-                'name' => 'JBL EON 610/710',
+                'name' => 'Enceinte JBL EON 610/710',
                 'slug' => 'jbl-eon-610-710',
                 'category' => 'Sonorisation',
-                'short_description' => 'Enceinte amplifiée, large bande active',
+                'short_description' => 'Enceinte amplifiée, large bande active.',
                 'description' => 'L\'enceinte JBL EON610/710, apporte une puissance de qualité professionnelle et une polyvalence sans précédent aux systèmes de sonorisation pour les applications live et les installations fixes. Son volume et sa clarté sonore associés à des fonctionnalités complètes rendent sa configuration et son utilisation très intuitives.',
                 'image' => '/IRCI/JBL EON 610-710.jpg',
                 'features' => [
@@ -83,10 +83,10 @@ class RentalController extends AbstractController
                 ],
             ],
             'jbl-control-25' => [
-                'name' => 'JBL Control 25',
+                'name' => 'Enceintes JBL Control 25',
                 'slug' => 'jbl-control-25',
                 'category' => 'Sonorisation',
-                'short_description' => 'Enceinte passive et compacte',
+                'short_description' => 'Enceinte passive et compacte.',
                 'description' => 'La JBL Control 25 est équipée d\'un support mural, elle est compacte et peut être déployée n\'importe où. Sa petite taille lui permet de se fondre dans le décor. Avec sa large diffusion elle couvrira parfaitement votre salle de conférence. Par paire avec un ampli.',
                 'image' => '/IRCI/JBL Control 25.jpg',
                 'features' => [
@@ -98,10 +98,10 @@ class RentalController extends AbstractController
                 ],
             ],
             'behringer-mpa-200bt' => [
-                'name' => 'Behringer MPA 200BT',
+                'name' => 'Enceinte Behringer MPA 200BT',
                 'slug' => 'behringer-mpa-200bt',
                 'category' => 'Sonorisation',
-                'short_description' => 'Enceinte compacte bluetooth sur batterie',
+                'short_description' => 'Enceinte compacte bluetooth sur batterie.',
                 'description' => 'La Behringer MPA 200BT fonctionne sur batterie (jusqu\'à 10h d\'autonomie), elle est facile à transporter et peut fonctionner en bluetooth. Sa puissance de 200W la rend idéale pour un cocktail ou une remise de prix.',
                 'image' => '/IRCI/Behringer MPA 200BT.jpg',
                 'features' => [
@@ -115,10 +115,10 @@ class RentalController extends AbstractController
                 ],
             ],
             'behringer-xenyx-x1204' => [
-                'name' => 'Behringer Xenyx X1204USB',
+                'name' => 'Console Behringer Xenyx X1204USB',
                 'slug' => 'behringer-xenyx-x1204',
                 'category' => 'Sonorisation',
-                'short_description' => 'Console compacte 12 entrées',
+                'short_description' => 'Console compacte 12 entrées.',
                 'description' => 'La table de mixage compacte X1204USB vous permet d\'obtenir sans effort un son de qualité supérieure, grâce ses 4 préamplis micro XENYX, ses égaliseurs, ses compresseurs et son processeur Multi-FX. Solution idéale pour vos concerts et vos enregistrements, la X1204USB est un mélangeur incroyablement polyvalent.',
                 'image' => '/IRCI/Behringer Xenyx X1204USB.jpg', // Image manquante
                 'features' => [
@@ -133,10 +133,10 @@ class RentalController extends AbstractController
                 ],
             ],
             'soundcraft-efx12' => [
-                'name' => 'Soundcraft EFX12',
+                'name' => 'Console Soundcraft EFX12',
                 'slug' => 'soundcraft-efx12',
                 'category' => 'Sonorisation',
-                'short_description' => 'Console analogique 14 entrées',
+                'short_description' => 'Console analogique 14 entrées.',
                 'description' => 'Console analogique 14 entrées, elle est légère est facilement transportable.',
                 'image' => '/IRCI/Soundcraft EFX12.jpg',
                 'features' => [
@@ -149,10 +149,10 @@ class RentalController extends AbstractController
                 ],
             ],
             'soundcraft-m8' => [
-                'name' => 'Soundcraft M8',
+                'name' => 'Console Soundcraft M8',
                 'slug' => 'soundcraft-m8',
                 'category' => 'Sonorisation',
-                'short_description' => 'Console 12 voies',
+                'short_description' => 'Console 12 voies.',
                 'description' => 'Grâce à la qualité de ses préamplis micro et à sa simplicité d\'utilisation, cette console de mixage polyvalente est à l\'aise aussi bien avec le mixage des micros utilisés lors des conférences et réunions, qu\'avec celui de chants et d\'instruments.',
                 'image' => '/IRCI/Soundcraft M8.jpg',
                 'features' => [
@@ -169,7 +169,7 @@ class RentalController extends AbstractController
                 'name' => 'Micros HF Shure Analogique',
                 'slug' => 'micros-shure-analogique',
                 'category' => 'Sonorisation',
-                'short_description' => 'Micro HF cardioïde SM58',
+                'short_description' => 'Micro HF cardioïde SM58.',
                 'description' => 'Premier choix des musiciens dans le monde, le SM58 est le microphone le plus polyvalent et le plus fiable de sa catégorie. Il isolera la voix de celui qui l\'utilise des bruits alentours. Résistant et fiable il est une valeur sûre. Grâce à ses plages de fréquences libres et son récepteur compact, il peut être utilisé en toute circonstance.',
                 'image' => '/IRCI/Micro Shure Analogique.jpg',
                 'features' => [
@@ -184,7 +184,7 @@ class RentalController extends AbstractController
                 ],
             ],
             'pack-micros-shure-numeriques' => [
-                'name' => 'Pack micros HF Shure numériques',
+                'name' => 'Pack de micros HF Shure numérique',
                 'slug' => 'pack-micros-shure-numeriques',
                 'category' => 'Sonorisation',
                 'short_description' => 'Micros montés en pack avec manager de fréquence. Idéal pour des tables rondes',
@@ -207,7 +207,7 @@ class RentalController extends AbstractController
                 'name' => 'Micro serre-tête Shure SM35 TQG',
                 'slug' => 'micro-serre-tete-shure-sm35',
                 'category' => 'Sonorisation',
-                'short_description' => 'Pour une utilisation main libre et une qualité inchangée',
+                'short_description' => 'Pour une utilisation main libre et une qualité inchangée.',
                 'description' => 'Certaines personnes préfèrent avoir les mains libres pour s\'exprimer, avec nos micros Shure SM35, nous équipons chaque personne pour qu\'elle se sente à l\'aise. Avec son émetteur de poche léger il se glisse facilement à la ceinture ou autre.',
                 'image' => '/IRCI/Micros serre tete.jpg',
                 'features' => [
@@ -221,7 +221,7 @@ class RentalController extends AbstractController
                 'name' => 'Micro cravate Shure CVL',
                 'slug' => 'micro-cravate-shure-cvl',
                 'category' => 'Sonorisation',
-                'short_description' => 'Pour une utilisation main libre et une qualité inchangée',
+                'short_description' => 'Pour une utilisation main libre et une qualité inchangée.',
                 'description' => 'Certaines personnes préfèrent avoir les mains libres pour s\'exprimer, avec nos micros Shure CVL, nous équipons chaque personne pour qu\'elle se sente à l\'aise. Avec son émetteur de poche léger il se glisse facilement à la ceinture ou autre.',
                 'image' => '/IRCI/Micros serre tete.jpg',
                 'features' => [
@@ -235,7 +235,7 @@ class RentalController extends AbstractController
                 'name' => 'Rack son clé en main',
                 'slug' => 'rack-son-cle-en-main',
                 'category' => 'Sonorisation',
-                'short_description' => 'Rack son équipé, il est conçu pour être autonome',
+                'short_description' => 'Rack son équipé, il est conçu pour être autonome.',
                 'description' => 'Nos racks son clé en main sont équipés d\'une console Behringer Xenyx X1222 avec 12 entrées, d\'un pack de 5 micros HF Shure numériques (possibilité de micro serre-tête ou cravate), d\'un manager de fréquence, d\'amplis et de 4 enceintes JBL Control 25. Fixez les enceintes, branchez-les, mettez du jus et c\'est parti.',
                 'image' => '/IRCI/Rack son cle en main.jpg', // Image manquante
                 'features' => [
@@ -249,10 +249,10 @@ class RentalController extends AbstractController
             ],
             // LUMIÈRE
             'eurolite-pfe60' => [
-                'name' => 'Eurolite PFE60 WW/CW Pro. Sport',
+                'name' => 'Projecteur Eurolite PFE60 WW/CW Pro. Sport',
                 'slug' => 'eurolite-pfe60',
                 'category' => 'Lumière',
-                'short_description' => 'Projecteurs à découpe, plusieurs angles de faisceaux disponibles',
+                'short_description' => 'Projecteurs à découpe, plusieurs angles de faisceaux disponibles.',
                 'description' => 'Cet appareil est un pro de la LEDfile spot pour des effets d\'éclairage professionnels dans le domaine de la technologie événementielle, par exemple sur scène. Parfait pour mettre en valeur votre évènement.',
                 'image' => '/IRCI/Eurolite PFE60.jpg',
                 'features' => [
@@ -265,7 +265,7 @@ class RentalController extends AbstractController
                 ],
             ],
             'eurolite-bar-12-qcl' => [
-                'name' => 'Eurolite bar-12 QCL',
+                'name' => 'Barre LED Eurolite bar-12 QCL',
                 'slug' => 'eurolite-bar-12-qcl',
                 'category' => 'Lumière',
                 'short_description' => 'Barre de 12 LED, couleur personnalisable à souhait.',
@@ -284,7 +284,7 @@ class RentalController extends AbstractController
                 'name' => 'Stairville DDC-12 LCD DMX Controller',
                 'slug' => 'stairville-ddc-12-lcd-dmx',
                 'category' => 'Lumière',
-                'short_description' => 'Pupitre DMX 12 canaux',
+                'short_description' => 'Pupitre DMX 12 canaux.',
                 'description' => 'Boitier de contrôle DMX 12 canaux.',
                 'image' => '/IRCI/Stairville DMX Controller.jpg',
                 'features' => [
@@ -302,7 +302,7 @@ class RentalController extends AbstractController
                 'name' => 'Télévisions Samsung',
                 'slug' => 'televisions-samsung',
                 'category' => 'Multimédia',
-                'short_description' => 'Ecrans de la marque Samsung exclusivement pour homogénéiser vos espaces',
+                'short_description' => 'Ecrans de la marque Samsung exclusivement pour homogénéiser vos espaces.',
                 'description' => 'Nous disposons d\'écrans de différentes tailles, allant du 24\'\' au 70\'\'. Les télévisions sont UHD 4K, elles peuvent être utilisés sur un meuble, une table mais également sur pied (hauteur de support réglable) ou en fixation murale.',
                 'image' => '/IRCI/TV Samsung.jpg', // Image manquante
                 'features' => [
@@ -313,10 +313,10 @@ class RentalController extends AbstractController
                 ],
             ],
             'videoprojecteur-courte-focale' => [
-                'name' => 'Vidéoprojecteur courte focale',
+                'name' => 'Vidéoprojecteur à courte focale',
                 'slug' => 'videoprojecteur-courte-focale',
                 'category' => 'Multimédia',
-                'short_description' => 'Vidéoprojecteur FullHD de 3600 à 4000 lumens',
+                'short_description' => 'Vidéoprojecteur FullHD de 3600 à 4000 lumens.',
                 'description' => 'Ces vidéoprojecteurs de résolution Full HD 1080p sont compatible HDR, offrant un taux de contraste de 50 000:1 et un niveau de luminosité maximal de 4 000 lumens.',
                 'image' => '/IRCI/VP courte focale.jpeg',
                 'features' => [
@@ -329,7 +329,7 @@ class RentalController extends AbstractController
                 'name' => 'Vidéoprojecteur haute résolution',
                 'slug' => 'videoprojecteur-haute-resolution',
                 'category' => 'Multimédia',
-                'short_description' => 'Projecteur LCD à partir de 5000 lumens',
+                'short_description' => 'Projecteur LCD à partir de 5000 lumens.',
                 'description' => 'Avec une luminosité élevée comprise entre 5 000 et 5 500 lm, la gamme vous promet ainsi des noirs profonds, un graphisme net et précis, et des textes faciles à déchiffrer.',
                 'image' => '/IRCI/VP haute résolution.jpeg',
                 'features' => [
@@ -342,7 +342,7 @@ class RentalController extends AbstractController
                 'name' => 'Vidéoprojecteur longue distance',
                 'slug' => 'videoprojecteur-longue-distance',
                 'category' => 'Multimédia',
-                'short_description' => 'Projecteur longue distance haute luminosité (7000 lumens)',
+                'short_description' => 'Projecteur longue distance haute luminosité (7000 lumens).',
                 'description' => 'Nos vidéoprojecteurs longue distance permettent de projeter à plus d\'une dizaine de mètres sans perte de qualité.',
                 'image' => '/IRCI/VP longue distance.jpg',
                 'features' => [
@@ -356,7 +356,7 @@ class RentalController extends AbstractController
                 'name' => 'Tablettes tactiles',
                 'slug' => 'tablettes-tactiles',
                 'category' => 'Multimédia',
-                'short_description' => 'Tablettes Apple ou Samsung selon préférence',
+                'short_description' => 'Tablettes Apple ou Samsung selon préférence.',
                 'description' => 'Nos tablettes 10 ou 11 pouces équipées de coques de protections peuvent être utilisées avec des supports sur pied ou sur table.',
                 'image' => '/IRCI/Tablettes tactiles.jpeg',
                 'features' => [
@@ -368,7 +368,7 @@ class RentalController extends AbstractController
                 'name' => 'Ordinateurs portables',
                 'slug' => 'ordinateurs-portables',
                 'category' => 'Multimédia',
-                'short_description' => 'Ordinateurs sous Windows avec un processeur allant de i3 à i7 avec pack office',
+                'short_description' => 'Ordinateurs sous Windows avec un processeur allant de i3 à i7 avec pack office.',
                 'description' => 'Nous avons différents ordinateurs pour s\'adapter au mieux à votre besoin. Ils sont tous équipés de cadenas.',
                 'image' => '/IRCI/Ordinateurs.webp',
                 'features' => [
@@ -382,7 +382,7 @@ class RentalController extends AbstractController
                 'name' => 'Lecteur CS 4070',
                 'slug' => 'lecteur-cs-4070',
                 'category' => 'Contrôle d\'accès',
-                'short_description' => 'Douchette de contrôle d\'accès compact',
+                'short_description' => 'Douchette de contrôle d\'accès compact.',
                 'description' => 'Les douchettes CS 4070 permettent un enregistrement des badges et une récupération des données. Les datas sont stockés sur la douchette avant d\'être récupérées.',
                 'image' => '/IRCI/CS4070.png',
                 'features' => [
@@ -395,7 +395,7 @@ class RentalController extends AbstractController
                 'name' => 'PDA Zebra TC21',
                 'slug' => 'pda-zebra-tc21',
                 'category' => 'Contrôle d\'accès',
-                'short_description' => 'Matériel de contrôle d\'accès en temps réel',
+                'short_description' => 'Matériel de contrôle d\'accès en temps réel.',
                 'description' => 'Zebra TC21 est un outil professionnel Android économique créé pour les petites et grandes entreprises, avec toutes les fonctionnalités d\'un smartphone et en plus un scanner code à barre capable de déchiffrer sur écran et papier et bien d\'autres. La Zebra TC21 permet un contrôle en direct des accès avec la possibilité de filtrer les participants.',
                 'image' => '/IRCI/Zebra TC21.jpg',
                 'features' => [
@@ -406,10 +406,10 @@ class RentalController extends AbstractController
                 ],
             ],
             'imprimante-multifonction-laser-hp' => [
-                'name' => 'Imprimante multifonction laser HP',
+                'name' => 'Imprimantes multifonction laser HP',
                 'slug' => 'imprimante-multifonction-laser-hp',
                 'category' => 'Contrôle d\'accès',
-                'short_description' => 'Imprimante couleur avec fonction copie et scanner inclus',
+                'short_description' => 'Imprimante couleur avec fonction copie et scanner inclus.',
                 'description' => 'Créez des documents couleur de qualité professionnelle et effectuez rapidement des tâches grâce à l\'impression recto verso ultrarapide.',
                 'image' => '/IRCI/Imprimante multifonction.jpeg',
                 'features' => [
@@ -423,10 +423,10 @@ class RentalController extends AbstractController
                 ],
             ],
             'imprimante-laser-noir-blanc' => [
-                'name' => 'Imprimante laser noir et blanc',
+                'name' => 'Imprimantes laser noir et blanc',
                 'slug' => 'imprimante-laser-noir-blanc',
                 'category' => 'Contrôle d\'accès',
-                'short_description' => 'Imprimante laser compacte',
+                'short_description' => 'Imprimante laser compacte.',
                 'description' => 'Nos imprimantes laser monochrome sont conçues pour optimiser la productivité. Avec une vitesse d\'impression allant jusqu\'à 32 pages par minute et la capacité d\'imprimer en recto-verso automatiquement à une vitesse de 16 faces par minute, elle répond aux besoins des professionnels les plus exigeants.',
                 'image' => '/IRCI/Imprimante laser noir et blanc.jpeg',
                 'features' => [
